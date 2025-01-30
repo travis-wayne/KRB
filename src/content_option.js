6cd4cffcdc665a630912c6fc198427c878ec9d75
Travis Wayne <joshuaxp9@gmail.com>
2025-01-29 19:08:21 -0800

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "KRB";
+const meta = {
+    title: "Kenechuku Richard Benjamin",
+    description: "I'm Kenechuku Richard Benjamin, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hello World, I'm Kenechuku Richard Benjamin", 
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Ethical Hacker",
+        fourth: "Programmer",
+        fifth: "Networking Expert",
+        sixth: "Analyst",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "Hello! I'm Kenechuku Richard Benjamin, a dedicated cybersecurity professional with a strong passion for safeguarding digital environments. With a solid foundation in security protocols and threat management, I specialize in identifying vulnerabilities and implementing effective strategies to protect systems from potential attacks. In addition to my cybersecurity expertise, I also have a keen interest and skills in basic web development, enjoying the process of creating secure, functional, and user-friendly websites. I combine my security and development experience to provide comprehensive solutions for clients and organizations, and I’m committed to continuous learning to stay up-to-date with the latest trends in both fields. Let’s connect and discuss how I can help you secure and optimize your online presence!",
+};
+const worktimeline = [{
+        jobtitle: "Example 1",
+        where: "Google", 
+        date: "2023",
+    },
+    {
+        jobtitle: "Example 2",
+        where: "Micosoft",
+        date: "2022",
+    },
+    {
+        jobtitle: "Example 3",
+        where: "Meta",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+    },
+    
+    {
+        name: "CSS",
+    },
+    
+    {
+        name: "JavaScript",
+    },
+    
+    {
+        name: "Tainwind CSS",
+    },
+
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Reverse Engineering",
+    },
+
+    {
+        name: "Web-Exploitation",
+    },
+
+    {
+        name: "Network Exploitation",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/500/",
+        description: "GDORKER-py is a Python-based command-line utility that leverages Google search operators...",
+        link: "https://github.com/travis-wayne/GDORKER---py",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "A complete redesign of AFIT student portal with Multifactor Authentication.",
+        link: "https://github.com/RichardBenjamin/AFIT-Portal",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "An online platform where pot artist can advertise and sell their clay creations and explore new ideas about pottery.",
+        link: "https://github.com/RichardBenjamin/PotterPalleteFB",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "A collaborative joint development on a webapp where technicains meet the consumers. It is a very seemless intergration.",
+        link: "https://github.com/travis-wayne/techconnet",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "A clone app the gets picyures from unsplash API.",
+        link: "https://github.com/travis-wayne/Unsplash-clone",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "okekekenechukwu281@gmail.com",
+    description: "Looking forward to hearing from you soon! 👌",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/RichardBenjamin",
+    linkedin: "https://www.linkedin.com/in/kenechukwu-okeke-295397290",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
